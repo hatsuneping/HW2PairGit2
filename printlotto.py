693108e49c2151a1d11cda515d44c04dcfddd124
Koshapop Sirorungsi <si.koshapop_st@tni.ac.th>
2016-12-16 04:12:40 +0000

    third commit

--- printlotto.py
+++ printlotto.py
@@ -12,5 +12,11 @@ lotteryNumbers.sort()
 print(">>> Today's lottery numbers are: ") 
 print(lotteryNumbers)
 
-
-
+r = random.randint(111,999)
+s = random.randint(111,999)
+t = random.randint(111,999)
+u = random.randint(111,999)
+print "%03d" % r
+print "%03d" % s
+print "%03d" % t
+print "%03d" % u
