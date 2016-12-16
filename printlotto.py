6c50740f8af0c0f0834a9864c111fa4716af86e6
Pacharaphol Kongnil <ko.pacharaphol_st@tni.ac.th>
2016-12-16 04:07:02 +0000

    second commit

--- printlotto.py
+++ printlotto.py
@@ -1,2 +1,16 @@
 
-jkhuiohuiibo
+
+import random
+
+lotteryNumbers = []
+for i in range (0,6):
+  number = random.randint(0,9)
+  while number in lotteryNumbers:
+    number = random.randint(0,9)
+  lotteryNumbers.append(number)
+lotteryNumbers.sort()
+print(">>> Today's lottery numbers are: ") 
+print(lotteryNumbers)
+
+
+
